e206f002d8cf75009c1d34169c036ac2ab5c1cde
yanfengofliu <47981916+yanfengofliu@users.noreply.github.com>
2019-02-27 16:44:50 +0800

    111
    11

--- learn_quartz/src/test/java/com/dufy/test/QuartzTest.java
+++ learn_quartz/src/test/java/com/dufy/test/QuartzTest.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 
 
 public class QuartzTest {
-	//个人测试
+	//个人测试测试个人
 	static Logger log = LoggerFactory.getLogger(QuartzTest.class);
 	
 	public static void main(String[] args) {

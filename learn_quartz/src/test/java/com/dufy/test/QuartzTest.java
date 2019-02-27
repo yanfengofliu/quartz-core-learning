2a0ddaa73d2ed04480fa65db8d849688b4988e9e
yanfengofliu <1603840933@qq.com>
2019-02-27 16:46:26 +0800

    不同冲突

--- learn_quartz/src/test/java/com/dufy/test/QuartzTest.java
+++ learn_quartz/src/test/java/com/dufy/test/QuartzTest.java
@@ -22,7 +22,7 @@ import org.slf4j.LoggerFactory;
 public class QuartzTest {
 	//个人测试
 	static Logger log = LoggerFactory.getLogger(QuartzTest.class);
-	
+	//你好
 	public static void main(String[] args) {
 		testStart();
 	}
